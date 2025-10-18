645021e3048fbaa98869f2a024bac0135f5175fa
Tetsuro Mikami <tetsuro@mickamy.com>
2025-10-18 11:43:06 +0900

    example: table driven test

--- example/internal/domain/session/handler/session_handler_test.go
+++ example/internal/domain/session/handler/session_handler_test.go
@@ -6,40 +6,59 @@ import (
 
 	"github.com/brianvoe/gofakeit/v7"
 	"github.com/mickamy/gokitx/either"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/mickamy/connecttest"
 	authv1 "github.com/mickamy/connecttest-example/gen/auth/v1"
 	"github.com/mickamy/connecttest-example/gen/github.com/mickamy/connecttest-example/gen/auth/v1/authv1connect"
 	"github.com/mickamy/connecttest-example/internal/domain/session/handler"
+	"github.com/mickamy/connecttest-example/internal/domain/session/usecase"
 )
 
 func TestSession_SignIn(t *testing.T) {
 	t.Parallel()
 
-	var out authv1.SignInResponse
-	connecttest.
-		New(t, either.Right(authv1connect.NewSessionServiceHandler(handler.NewSession()))).
-		Procedure(authv1connect.SessionServiceSignInProcedure).
-		In(&authv1.SignInRequest{
-			Email:    gofakeit.Email(),
-			Password: gofakeit.Password(true, true, true, true, false, 12),
-		}).
-		Do().
-		ExpectStatus(http.StatusOK).
-		Out(&out)
-
-	if out.Tokens == nil {
-		t.Fatal("expected tokens to be set")
+	tcs := []struct {
+		name      string
+		uc        usecase.CreateSession
+		wantCode  int
+		assertRes func(t *testing.T, res *authv1.SignInResponse)
+		assertErr func(t *testing.T, err error)
+	}{
+		{
+			name:     "success",
+			uc:       usecase.NewCreateSession(),
+			wantCode: http.StatusOK,
+			assertRes: func(t *testing.T, res *authv1.SignInResponse) {
+				require.NotZero(t, res.Tokens)
+				assert.NotZero(t, res.Tokens.Access)
+				assert.NotZero(t, res.Tokens.Refresh)
+			},
+			assertErr: func(t *testing.T, err error) {
+				require.NoError(t, err)
+			},
+		},
 	}
 
-	if out.Tokens.Access == nil {
-		t.Fatal("expected access token to be set")
-	}
+	for _, tc := range tcs {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-	if out.Tokens.Refresh == nil {
-		t.Fatal("expected refresh token to be set")
-	}
+			var out authv1.SignInResponse
+			connecttest.
+				New(t, either.Right(authv1connect.NewSessionServiceHandler(handler.NewSession(tc.uc)))).
+				Procedure(authv1connect.SessionServiceSignInProcedure).
+				In(&authv1.SignInRequest{
+					Email:    gofakeit.Email(),
+					Password: gofakeit.Password(true, true, true, true, false, 12),
+				}).
+				Do().
+				ExpectStatus(http.StatusOK).
+				Out(&out)
 
-	t.Logf("Access Token: %s", out.Tokens.Access.Value)
-	t.Logf("Refresh Token: %s", out.Tokens.Refresh.Value)
+			tc.assertRes(t, &out)
+		})
+	}
 }
